3f472c890a9c964b5cc370d011994d7dd27d5d85
kossadda <gabilov1997@gmail.com>
2024-12-17 16:49:15 +0500

    feat(share): add custom logger for local debug

--- share/go.mod
+++ share/go.mod
@@ -3,14 +3,18 @@ module github.com/kossadda/wallet-exchanger/share
 go 1.23.3
 
 require (
+	github.com/fatih/color v1.18.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	golang.org/x/exp v0.0.0-20241215155358-4a5509556b9e
 	google.golang.org/grpc v1.69.0
 	google.golang.org/protobuf v1.35.2
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
--- share/logger/logger.go
+++ share/logger/logger.go
@@ -1,8 +1,14 @@
 package logger
 
 import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
 	"log/slog"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 const (
@@ -11,23 +17,124 @@ const (
 	envProd  = "prod"
 )
 
-func Setup(env string) *slog.Logger {
-	var log *slog.Logger
+type HandlerOptions struct {
+	SlogOpts *slog.HandlerOptions
+}
+
+type Handler struct {
+	opts HandlerOptions
+	slog.Handler
+	l     *log.Logger
+	attrs []slog.Attr
+}
+
+func (opts HandlerOptions) NewHandler(out io.Writer) *Handler {
+	h := &Handler{
+		Handler: slog.NewJSONHandler(out, opts.SlogOpts),
+		l:       log.New(out, "", 0),
+	}
+
+	return h
+}
+
+func (h *Handler) Handle(_ context.Context, r slog.Record) error {
+	level := r.Level.String() + ":"
+
+	switch r.Level {
+	case slog.LevelDebug:
+		level = color.MagentaString(level)
+	case slog.LevelInfo:
+		level = color.BlueString(level)
+	case slog.LevelWarn:
+		level = color.YellowString(level)
+	case slog.LevelError:
+		level = color.RedString(level)
+	}
+
+	fields := make(map[string]interface{}, r.NumAttrs())
+
+	r.Attrs(func(a slog.Attr) bool {
+		fields[a.Key] = a.Value.Any()
+
+		return true
+	})
+
+	for _, a := range h.attrs {
+		fields[a.Key] = a.Value.Any()
+	}
+
+	var b []byte
+	var err error
+
+	if len(fields) > 0 {
+		b, err = json.MarshalIndent(fields, "", "  ")
+		if err != nil {
+			return err
+		}
+	}
+
+	timeStr := r.Time.Format("[15:05:05.000]")
+	msg := color.CyanString(r.Message)
+
+	h.l.Println(
+		timeStr,
+		level,
+		msg,
+		color.WhiteString(string(b)),
+	)
+
+	return nil
+}
+
+func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &Handler{
+		Handler: h.Handler,
+		l:       h.l,
+		attrs:   attrs,
+	}
+}
+
+func (h *Handler) WithGroup(name string) slog.Handler {
+	return &Handler{
+		Handler: h.Handler.WithGroup(name),
+		l:       h.l,
+	}
+}
+
+func SetupLogger() *slog.Logger {
+	opts := HandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewHandler(os.Stdout)
+
+	return slog.New(handler)
+}
+
+func SetupByEnv(env string) *slog.Logger {
+	var lg *slog.Logger
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		lg = SetupLogger()
 	case envDev:
-		log = slog.New(
+		lg = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
 	case envProd:
-		log = slog.New(
+		lg = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
 	}
 
-	return log
+	return lg
+}
+
+func Err(err error) slog.Attr {
+	return slog.Attr{
+		Key:   "error",
+		Value: slog.StringValue(err.Error()),
+	}
 }
